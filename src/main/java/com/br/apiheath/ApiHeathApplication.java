ac4d0be442fd2c6950c74b6420223ca759836853
Gabriel Rodrigues <gabrielrodrigues1927@gmail.com>
2023-11-20 10:16:25 -0300

    init

---
+++ src/main/java/com/br/apiheath/ApiHeathApplication.java
@@ -0,0 +1,13 @@
+package com.br.apiheath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHeathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHeathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/apiheath/ApiHeathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.apiheath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHeathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
