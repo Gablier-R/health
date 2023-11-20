9ec1309365b7777b02772fdbb3295f7c9391a870
Gabriel Rodrigues <gabrielrodrigues1927@gmail.com>
2023-11-20 19:56:19 -0300

    adding health check

--- src/main/java/com/br/apiheath/api/controller/HealthCheckController.java
+++ src/main/java/com/br/apiheath/api/controller/HealthCheckController.java
@@ -1,12 +1,14 @@
 package com.br.apiheath.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/health")
 public class HealthCheckController {
 
-    @GetMapping("/health")
+    @GetMapping
     public String healthCheck() {
         return "OK";
     }
